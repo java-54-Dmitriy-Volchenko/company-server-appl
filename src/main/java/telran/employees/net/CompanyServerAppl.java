99f6711dbcac17d8edade7231ccfc926c3afd7b4
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-08-02 15:52:59 +0300

    teachers version and HW start

--- src/main/java/telran/employees/net/CompanyServerAppl.java
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -1,50 +1,50 @@
 package telran.employees.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import telran.employees.*;
 import telran.io.Persistable;
 import telran.net.Protocol;
 import telran.net.TcpServer;
-
-public class CompanyServerAppl {
+import static telran.net.TcpConfigurationProperties.*
+;public class CompanyServerAppl {
 
 	private static final String FILE_NAME = "employeesTest.data";
 	private static final int PORT = 5001;
-
+	
 	public static void main(String[] args) {
 		
 		Company company = new CompanyMapsImpl();
-		try {
-			((Persistable)company).restore(FILE_NAME);
-		} catch (Exception e) {
-			
+		Persistable persistable = null;
+		if(company instanceof Persistable) {
+			persistable = (Persistable)company;
 		}
-		
+		if (persistable != null) {
+			try {
+				persistable.restore(FILE_NAME);
+			} catch (Exception e) {
+
+			} 
+		}
 		Protocol protocol = new CompanyProtocol(company);
 		TcpServer tcpServer = new TcpServer(protocol, PORT);
-		new Thread(() -> tcpServer.run()).start();
-		 try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-	            while (true) {
-	                System.out.println("Enter 'shutdown' to stop the server:");
-	                String command = reader.readLine();
-	                if ("shutdown".equalsIgnoreCase(command)) {
-	                    tcpServer.shutdown();
-	                    
-	                    break;
-	                }
-	            }
-	        } catch (IOException e) {
-	         
-	        }
+		Thread serverThread = new Thread(tcpServer);
+		serverThread.start();
+		boolean running = true;
+		try (Scanner scanner = new Scanner(System.in);){
+			while(running) {
+				System.out.println(String.format("Enter %s for the server shutdown", SHUTDOWN));
+				String line = scanner.nextLine();
+				if (line != null && line.equals(SHUTDOWN)) {
+					running = false;
+					tcpServer.shutdown();
+					if (persistable != null) {
+						persistable.save(FILE_NAME);
+					}
+				}
+			}
+		}
 
-	
-	try {
-        ((Persistable)company).save(FILE_NAME);
-		} catch (Exception e) {
-       
-    }
-  }
+	}
+
 }
